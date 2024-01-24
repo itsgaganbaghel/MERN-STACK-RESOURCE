d0dd448e433740cbbe58e0943eb1c8e7f31bc1aa
itsgaganbaghel <gagan717114baghel@gmail.com>
2024-01-24 23:59:15 +0530

    K

--- front-end/04-JavaScript/Projects/02-share-modal/index.js
+++ front-end/04-JavaScript/Projects/02-share-modal/index.js
@@ -1,23 +1,30 @@
-// modal - social media 
+// // modal - social media 
 
 
-// const modal = document.querySelector(".container");
-const card = document.querySelector("#card") ;
+// // const modal = document.querySelector(".container");
+// const card = document.querySelector("#card") ;
 
-console.log(card);
-// console.log(overlay);
+// console.log(card);
+// // console.log(overlay);
+
+// const openModal = () => {
+//     console.log("modal is open");
+//     card.classList.add("active")
+
+//     // card.style.scale = '1.1';
+//     // overlay.classList.add("overlay");
+// }
+
+// // const closeModal = () => {
+//     // console.log("modal is close ");
+//     // card.style.scale = '0';
+
+//     // overlay.classList.remove("overlay");
+// // }
+
+const openTrigger =  document.querySelector(".heading");
+const modal = document.querySelector("#card");
 
 const openModal = () => {
-    console.log("modal is open");
-    card.classList.add("active")
-
-    // card.style.scale = '1.1';
-    // overlay.classList.add("overlay");
-}
-
-const closeModal = () => {
-    console.log("modal is close ");
-    // card.style.scale = '0';
-
-    // overlay.classList.remove("overlay");
+    modal.classList.add("active");
 }
