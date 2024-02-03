3e3009c8e23cb772e539e73a2c18a62f6f77b209
itsgaganbaghel <gagan717114baghel@gmail.com>
2024-02-04 04:05:41 +0530

    discord_server

---
+++ front-end/03-Tailwind_project/02-Discord_server_Clone/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontfamily:{
+    whitney:["whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
+
